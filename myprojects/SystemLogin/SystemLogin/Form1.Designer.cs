118d930af46c243a07113e0172ddf64b55de5b83
huykaiser <vogiahuy97@gmail.com>
2021-11-24 09:29:48 +0100

    Upload

---
+++ myprojects/SystemLogin/SystemLogin/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SystemLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbUser = new System.Windows.Forms.Label();
+            this.lbPass = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbUser
+            // 
+            this.lbUser.AutoSize = true;
+            this.lbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUser.Location = new System.Drawing.Point(36, 60);
+            this.lbUser.Name = "lbUser";
+            this.lbUser.Size = new System.Drawing.Size(139, 29);
+            this.lbUser.TabIndex = 0;
+            this.lbUser.Text = "Username:";
+            // 
+            // lbPass
+            // 
+            this.lbPass.AutoSize = true;
+            this.lbPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPass.Location = new System.Drawing.Point(36, 105);
+            this.lbPass.Name = "lbPass";
+            this.lbPass.Size = new System.Drawing.Size(135, 29);
+            this.lbPass.TabIndex = 1;
+            this.lbPass.Text = "Password:";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(222, 60);
+            this.tbUser.Multiline = true;
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(179, 29);
+            this.tbUser.TabIndex = 2;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(222, 105);
+            this.tbPass.Multiline = true;
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(179, 29);
+            this.tbPass.TabIndex = 3;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(64, 158);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(138, 49);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(242, 158);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(138, 49);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 248);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.lbPass);
+            this.Controls.Add(this.lbUser);
+            this.Name = "Form1";
+            this.Text = "Login System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbUser;
+        private System.Windows.Forms.Label lbPass;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ myprojects/SystemLogin/SystemLogin/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SystemLogin
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conn;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public Form1()
+        {
+            server = "localhost";
+            database = "tut";
+            uid = "root";
+            password = "123456789";
+
+            string connString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+
+            conn = new MySqlConnection(connString);
+
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string user = tbUser.Text;
+            string pass = tbPass.Text;
+
+            if (Register(user, pass))
+            {
+                MessageBox.Show($"User {user} has been created!");
+            }
+            else
+            {
+                MessageBox.Show($"User {user} has not been created!");
+            }
+        }
+
+        public bool Register(string user, string pass)
+        {
+            string query = $"INSERT INTO users (username,password) VALUES ('{user}','{pass}');";
+
+            try
+            {
+                if (OpenConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        //MySqlDataReader reader = cmd.ExecuteReader();
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        return false;
+                    }
+                }
+                else
+                {
+                    conn.Close();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conn.Close();
+                return false;
+            }
+        }
+
+        public bool IsLogin(string user, string pass)
+        {
+            string query = $"SELECT * FROM users WHERE username='{user}' AND password='{pass}'";
+
+            try
+            {
+                if (OpenConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        reader.Close();
+                        conn.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        conn.Close();
+                        return false;
+                    }
+                }
+                else
+                {
+                    conn.Close();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conn.Close();
+                return false;
+            }
+
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Connection to server failed!!!! plzz");
+                        break;
+                    case 1045:
+                        MessageBox.Show("Server username or password is incorrect!");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string user = tbUser.Text;
+            string pass = tbPass.Text;
+
+            if (IsLogin(user, pass))
+            {
+                MessageBox.Show($"Welcome {user}!!!");
+            }
+            else
+            {
+                MessageBox.Show($"{user} does not exist or password is incorrect!");
+            }
+        }
+    }
+}
